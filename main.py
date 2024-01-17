dbef2b3716e74c3d7145b937a1e06578ac859a45
carterdr <carterrobinson1123@gmail.com>
2024-01-16 21:33:12 -0500

    added final report

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 def main():
     path = "books/frankestein.txt"
     text = openFile(path)
-    print(getNumWords(text))
-    print(get_chars_dict(text))
-        
+    printReport(text, path)
 def getNumWords(text):
     return len(text.split())
 def openFile(path):
@@ -12,9 +10,20 @@ def openFile(path):
 def get_chars_dict(text):
     chars = {}
     for c in text:
-        if c.lower() not in chars:
-            chars[(c.lower())] = 1
-        else:    
-            chars[(c.lower())] += 1    
+        if c.isalpha():
+            if c.lower() not in chars:
+                chars[(c.lower())] = 1
+            else:    
+                chars[(c.lower())] += 1    
     return chars
+def printReport(text, path):
+    print(f"--- Begin report of {path} ---")
+    print(f"{getNumWords(text)} found in the document")
+    lis = []
+    for key, value in get_chars_dict(text).items():
+        lis.append((key, value))
+    lis.sort(key = lambda x: x[1], reverse = True)
+    for item in lis:
+        print(f"The {item[0]} character was found {item[1]} times")
+    print("--- End report ---")
 main()
