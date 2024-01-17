2a4ab507d21af6bc3493b261f5b6b6adf8eff4f5
carterdr <carterrobinson1123@gmail.com>
2024-01-16 21:22:22 -0500

    added word counter, file opener and character dictionary

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-with open("books/frankestein.txt") as f:
-    contents = f.read()
-    print(contents)
+def main():
+    path = "books/frankestein.txt"
+    text = openFile(path)
+    print(getNumWords(text))
+    print(get_chars_dict(text))
+        
+def getNumWords(text):
+    return len(text.split())
+def openFile(path):
+    with open(path) as f:
+        return f.read()
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        if c.lower() not in chars:
+            chars[(c.lower())] = 1
+        else:    
+            chars[(c.lower())] += 1    
+    return chars
+main()
